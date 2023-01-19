059b0097f953aaa5b0171f062ba70fe5ea551a96
Firgisotya <zzuuddiinn28@gmail.com>
2023-01-19 20:56:49 +0700

    update pertama

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const routes = require('./routes')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+routes(app);
 
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
